f656b893cc1f725d5091ae041ba9613c937e4e5a
Aseem <aseemanand2@gmail.com>
2020-10-31 17:27:59 +0530

    [Aseem] Added code to calculate total fare given travel distance and time

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace CabInvoiceGenerator
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Cab Invoice Generator!");
         }
     }
 }
---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Tests.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace NUnitTestProject1
+{
+    using CabInvoiceGenerator;
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        public InvoiceGenerator invoiceGenerator = null;
+
+        /// <summary>
+        /// UC 1 : Given the distance and time should return total fare.
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_ShouldReturnTotalFare()
+        {
+            // Arrange
+            double expectedFare = 70;
+            double distance = 5;
+            int minutes = 20;
+            invoiceGenerator = new InvoiceGenerator();
+            // Act
+            double actualFare = invoiceGenerator.CalculateFare(distance, minutes);
+            //Assert
+            Assert.AreEqual(expectedFare,actualFare);
+        }
+    }
+}
