84ace2de62e90411f9dc87bb7ad49d26022817de
Aseem <aseemanand2@gmail.com>
2020-10-31 20:30:17 +0530

    [Aseem] Added code to generate invoice for a particular userId

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -45,5 +45,31 @@ namespace NUnitTestProject1
             //Assert
             Assert.AreEqual(expectedSummary, actualSummary);
         }
+
+        /// <summary>
+        /// UC 4 : Given the user id, invoice service gets list of rides and returns invoice summary.
+        /// </summary>
+        [Test]
+        public void GivenUserId_InvoiceServiceGetsListOfRides_ShouldReturnInvoiceSummary()
+        {
+            // Arrange
+            invoiceGenerator = new InvoiceGenerator();
+            string userId1 = "USER1";
+            string userId2 = "USER2";
+            Ride[] rides1 = { new Ride(3, 5), new Ride(5, 4), new Ride(2, 5) };
+            Ride[] rides2 = { new Ride(0.1, 1), new Ride(0.1, 3), new Ride(0.1, 2) };
+            invoiceGenerator.AddRides(userId1, rides1);
+            invoiceGenerator.AddRides(userId2, rides2);
+            InvoiceSummary expectedSummary1 = new InvoiceSummary(3, 114, 38);
+            InvoiceSummary expectedSummary2 = new InvoiceSummary(3, 15, 5);
+
+            // Act
+            InvoiceSummary actualSummary1 = invoiceGenerator.GetInvoiceSummary(userId1);
+            InvoiceSummary actualSummary2 = invoiceGenerator.GetInvoiceSummary(userId2);
+
+            //Assert
+            Assert.AreEqual(expectedSummary1, actualSummary1);
+            Assert.AreEqual(expectedSummary2, actualSummary2);
+        }
     }
 }
